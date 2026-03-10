57f8c13ec8b81c4c14dc45d63decc06992ff8373
Aref <arefrafei92@gmail.com>
2026-03-10 12:38:46 -0700

    Add concurrently for parallel script execution; update index.ts to run server and client concurrently; add dev script to package.json

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+  {
+    name: "server",
+    command: "./run.sh",
+    prefixColor: "cyan",
+    cwd: "packages/server",
+  },
+  {
+    name: "client",
+    command: "bun run dev",
+    prefixColor: "magenta",
+    cwd: "packages/client",
+  },
+]);
