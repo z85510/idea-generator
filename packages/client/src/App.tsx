e923ca4f4a8f8637776dad0087d98d0639cdff79
Aref <arefrafei92@gmail.com>
2026-03-10 12:56:01 -0700

    setup shadcn

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Label } from "./components/ui/label";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -10,8 +12,17 @@ function App() {
   }, []);
 
   return (
-    <div className="text-2xl font-bold text-red-500 p-4 text-center">
-      {message}
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Label className="text-2xl font-bold text-red-500 p-4 text-center">
+        {message}
+      </Label>
+      <div className="flex flex-col gap-2">
+        <Button variant="outline">Click me (outline)</Button>
+        <Button variant="ghost">Click me (ghost)</Button>
+        <Button variant="secondary">Click me (secondary)</Button>
+        <Button variant="destructive">Click me (destructive)</Button>
+        <Button variant="link">Click me (link)</Button>
+      </div>
     </div>
   );
 }
